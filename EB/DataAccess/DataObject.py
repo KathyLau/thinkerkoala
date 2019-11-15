05fd4bc97857ee9a137ff85c217103f0322cd7a7
pelincetin <pc2807@barnard.edu>
2019-11-15 18:20:51 -0500

    making sure everything is upto date.

--- EB/DataAccess/DataObject.py
+++ EB/DataAccess/DataObject.py
@@ -2,7 +2,7 @@ import DataAccess.DataAdaptor as data_adaptor
 from abc import ABC, abstractmethod
 import pymysql.err
 
-database = "thinkerkoala"
+database = "e6156"
 
 class DataException(Exception):
 
